9551b7920e110f5e8b3cde7026d68b0b42c4c423
Satyam Singh <satyamrajput7239@gmail.com>
2025-09-05 15:51:44 +0530

    feat: add route to get data

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,17 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import portfolioData from "./data.js";
 
 const app = express();
 dotenv.config();
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/api/portfolio", (req, res) => {
+  res.json(portfolioData);
+});
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("Mongodb Connected");
